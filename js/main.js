13787d9c26c8417bfbbaff9295448eda3ac498e1
GLEB <gleb.severeniuk@yandex.ru>
2026-04-06 18:08:08 +0300

    Стилизация кнопок

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let title = "";
+if (title === ""){
+    console.log("НАзвание задачи не указана")
+} else {
+    console.log("Задача", title)
+}
+
+let taskCount = 5;
+if (taskCount === 0){
+    console.log("Список пуст")
+} else if (taskCount <= 0 ) {
+    console.log("")
+} else {
+    console.log("Задач очень много")
+}
